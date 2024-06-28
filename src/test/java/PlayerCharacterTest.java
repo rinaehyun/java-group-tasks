0b66cbd0d874d99ce0402d557e7e54b53831bf93
Marcel Herr <mrclhrr@gmail.com>
2024-06-28 16:13:42 +0200

    add getX test

--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -1,5 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerCharacterTest {
+
+    @Test
+    public void getXTest_whenStart_thenReturn0() {
+        //Given
+        boolean start = true;
+
+        //When
+        int actual = getX(start);
+
+        //Then
+        assertEquals(actual, 0);
+    }
 
 }
